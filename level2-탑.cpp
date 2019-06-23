39458fb4af1a04fe7fab53fe1e5c7ef29445b221
ksm357 <ksm3725@naver.com>
2019-06-23 16:28:03 +0900

    level 2 - 탑

---
+++ level2-탑.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> heights) {
+    vector<int> answer;
+    
+    for(int i=heights.size()-1; i>=0; i--) {
+        for(int j=i-1; j>=0; j--) {
+            if(heights[i] < heights[j]) {
+                answer.push_back(j+1);
+                break;
+            }
+            if(j==0) {
+                answer.push_back(0);
+            }
+        }
+    }
+    answer.push_back(0);
+    reverse(answer.begin(), answer.end());
+    return answer;
+}
