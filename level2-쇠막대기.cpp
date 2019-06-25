f149a89598d093c2d5e9130ba6f45fce2e6706dd
ksm357 <ksm3725@naver.com>
2019-06-25 23:26:59 +0900

    level 2 - 쇠막대기

---
+++ level2-쇠막대기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(string arrangement) {
+    int answer = 0;
+    stack<char> st;
+    for(int i=0; i<arrangement.length(); i++) {
+        if(arrangement[i]=='(') {
+            st.push(arrangement[i]);
+        } else {
+            if(!st.empty()) {
+                st.pop();
+                if(arrangement[i-1]=='(') {
+                    answer += st.size();
+                } else {
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
